5a250780eb41760cbe3871fa97baa4606e064028
Danielvictor715 <daniel.oliveira.batista715@gmail.com>
2025-10-03 00:47:52 +0000

    Criei o sistema de cadastro das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,72 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+char estado, estado2;
+int codigo, codigo2;
+char nome[20], nome2[20];
+int populacao, populacao2;
+float area, area2;
+float PIB, PIB2;
+int pontost, pontost2;
 
   // Área para entrada de dados
+
+printf("Bem vindo ao Super Trunfo de Paises\n");
+printf("Vamos cadastra a 1° carta!\n\n");
+
+// cadastro da primeira carta
+
+printf("Digite uma letra para representar a carta:\n");
+scanf(" %c", &estado);
+
+printf("Digite o código da carta:\n");
+scanf("%d", &codigo);
+
+printf("Digite o nome do Estado que representa a carta:\n");
+scanf(" %s", &nome);
+
+printf("Digite a quantidade da população:\n");
+scanf("%d", &populacao);
+
+printf("Digite a Area em km²:\n");
+scanf("%f", &area);
+
+printf("Digite o PIB:\n");
+scanf("%f", &PIB);
+
+printf("Digite a quantidade de pontos turisticos:\n");
+scanf("%d", &pontost);
+
+// cadastro da segunda carta
+
+printf("Vamos cadastrar a 2° carta!\n\n");
+
+printf("Bem vindo ao Super Trunfo de Paises\n");
+printf("Vamos cadastra a 1° carta!\n\n");
+
+// cadastro da primeira carta
+
+printf("Digite uma letra para representar a carta:\n");
+scanf(" %c", &estado2);
+
+printf("Digite o código da carta:\n");
+scanf("%d", &codigo2);
+
+printf("Digite o nome do Estado que representa a carta:\n");
+scanf(" %s", &nome2);
+
+printf("Digite a quantidade da população:\n");
+scanf("%d", &populacao2);
+
+printf("Digite a Area em km²:\n");
+scanf("%f", &area2);
+
+printf("Digite o PIB:\n");
+scanf("%f", &PIB2);
+
+printf("Digite a quantidade de pontos turisticos:\n");
+scanf("%d", &pontost2);
+
 
   // Área para exibição dos dados da cidade
 
